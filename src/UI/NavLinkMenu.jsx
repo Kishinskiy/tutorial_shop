b3cd5f0ffbd23c464898978604b10d2f39af5405
Oleg Kishinskiy <oleg.kishinskiy@gmail.com>
2026-01-08 03:50:52 +0300

    fix hover effect

--- src/UI/NavLinkMenu.jsx
+++ src/UI/NavLinkMenu.jsx
@@ -2,6 +2,6 @@ import {NavLink} from "react-router-dom";
 
 export default function NavLinkMenu({ to, children }) {
     return (
-        <NavLink className="text-2xl font-semibold text-blue-600 hover:text-red-400;" to={to}>{children}</NavLink>
+        <NavLink className="text-2xl font-semibold text-blue-600 hover:text-red-500" to={to}>{children}</NavLink>
     )
 }
