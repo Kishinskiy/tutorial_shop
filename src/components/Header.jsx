bb16c4f38829b7881f8307ab43693de9cbd42c19
Oleg Kishinskiy <oleg.kishinskiy@gmail.com>
2026-02-07 18:32:46 +0300

    add restart policy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export default function Header() {
 
     return (
         <>
-        <header className="bg-gradient-to-r from-blue-100 via-blue-400 to-blue-600 shadow-lg sticky top-0 z-50">
+        <header className="bg-linear-to-r from-blue-100 via-blue-400 to-blue-600 shadow-lg sticky top-0 z-50">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-20">
                     <Link to="/" className="flex items-center group">
